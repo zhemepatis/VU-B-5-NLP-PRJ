583f0c4eb1c228889fc40f1688073ea796cdf11a
zhemepatis <rink.gab@gmail.com>
2024-11-25 19:53:31 +0200

    added movies.csv data file prepocessing

---
+++ utilities/data_preprocessing.py
@@ -0,0 +1,26 @@
+import re
+
+
+def extract_movie_year(title_cell):
+    pattern = r"\((\d{4})\)"
+    match = re.search(pattern, title_cell)
+
+    if match:
+        return match.group(1)
+    
+    return None
+
+
+def extract_movie_genres(genres_cell):
+    genre_list = genres_cell.split("|")
+    return genre_list
+
+
+def clean_movie_title(title_cell):
+    pattern = r"([\w\W]+) \(\d{4}\)"
+    match = re.search(pattern, title_cell)
+
+    if match:
+        return match.group(1)
+    
+    return None
